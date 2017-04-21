e3e330f37c0c8888ff3155d4b168112ce0cfa2b8
gdunn1 <ghdcomp@hotmail.com>
2017-04-20 22:09:53 -0600

    Changes that were missed earlier as code was refactered, latest wasn't committed

--- WindowsFormsApplication10/Form1.cs
+++ WindowsFormsApplication10/Form1.cs
@@ -11,26 +11,25 @@ using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace WindowsFormsApplication10
 {
     public partial class Form1 : Form
     {
         static List<RepoDetails> repoList = new List<RepoDetails>();
-        static bool continueProcessing = true;
-        static string userName;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        static async void DownloadRepoInfoAsync()
+        private async Task DownloadRepoInfoAsync(string username)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://api.github.com/");
             client.DefaultRequestHeaders.Add("User-Agent", "Anything");
-            var response = await client.GetAsync("users/" + userName + "/repos");
+            var response = await client.GetAsync("users/" + username + "/repos");
             string responseBody = await response.Content.ReadAsStringAsync();
             var jsonResponse = JsonConvert.DeserializeObject(responseBody);
             dynamic responseDynamic = ((Newtonsoft.Json.Linq.JArray)jsonResponse);
@@ -44,35 +43,23 @@ namespace WindowsFormsApplication10
                 repoObj.CreateDate = (string)responseObj.SelectToken("created_at");
                 repoList.Add(repoObj);
             }
-            continueProcessing = false;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             lstRepoResults.Items.Clear();
-            userName = txtUserId.Text;
-            Task t = new Task(DownloadRepoInfoAsync);
-            t.Start();
+            await DownloadRepoInfoAsync(txtUserId.Text);
 
-            do
+            if (repoList.Count == 0)
+                lstRepoResults.Items.Add("No Repositories found for this user id!");
+            else
             {
-                if (!continueProcessing)
+                foreach (var item in repoList)
                 {
-                    if (repoList.Count == 0)
-                        lstRepoResults.Items.Add("No Repositories found for this user id!");
-                    else
-                    {
-                        foreach (var item in repoList)
-                        {
-                            string outputInfo = string.Format("Repo Name: {0}, Description: {1}, Created Date: {2}", item.Name, item.Description, item.CreateDate);
-                            lstRepoResults.Items.Add(outputInfo);
-                        }
-                    }
+                    string outputInfo = string.Format("Repo Name: {0}, Description: {1}, Created Date: {2}", item.Name, item.Description, item.CreateDate);
+                    lstRepoResults.Items.Add(outputInfo);
                 }
-            } while (continueProcessing);
-
-            t.Dispose();
-            continueProcessing = true;
+            }
             repoList.Clear();
         }
     }
