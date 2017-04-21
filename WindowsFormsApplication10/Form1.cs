4cb143b19ccc8a43c41a806a48c369cbb028764d
gdunn1 <ghdcomp@hotmail.com>
2017-04-21 07:58:10 -0600

    Another refractor

--- WindowsFormsApplication10/Form1.cs
+++ WindowsFormsApplication10/Form1.cs
@@ -17,50 +17,38 @@ namespace WindowsFormsApplication10
 {
     public partial class Form1 : Form
     {
-        static List<RepoDetails> repoList = new List<RepoDetails>();
-
+        
         public Form1()
         {
             InitializeComponent();
         }
 
-        private async Task DownloadRepoInfoAsync(string username)
+        private async Task<string> DownloadRepoInfoAsync(string username)
         {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://api.github.com/");
             client.DefaultRequestHeaders.Add("User-Agent", "Anything");
             var response = await client.GetAsync("users/" + username + "/repos");
             string responseBody = await response.Content.ReadAsStringAsync();
-            var jsonResponse = JsonConvert.DeserializeObject(responseBody);
-            dynamic responseDynamic = ((Newtonsoft.Json.Linq.JArray)jsonResponse);
-
-            foreach (var item in responseDynamic)
-            {
-                RepoDetails repoObj = new RepoDetails();
-                JObject responseObj = JObject.Parse(item.ToString());
-                repoObj.Name = (string)responseObj.SelectToken("name");
-                repoObj.Description = (string)responseObj.SelectToken("description");
-                repoObj.CreateDate = (string)responseObj.SelectToken("created_at");
-                repoList.Add(repoObj);
-            }
+            return responseBody;
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
             lstRepoResults.Items.Clear();
-            await DownloadRepoInfoAsync(txtUserId.Text);
+            string responseBody = await DownloadRepoInfoAsync(txtUserId.Text);
 
-            if (repoList.Count == 0)
-                lstRepoResults.Items.Add("No Repositories found for this user id!");
-            else
+            var jsonResponse = JsonConvert.DeserializeObject(responseBody);
+            dynamic responseDynamic = ((Newtonsoft.Json.Linq.JArray)jsonResponse);
+
+            foreach (var item in responseDynamic)
             {
-                foreach (var item in repoList)
-                {
-                    string outputInfo = string.Format("Repo Name: {0}, Description: {1}, Created Date: {2}", item.Name, item.Description, item.CreateDate);
-                    lstRepoResults.Items.Add(outputInfo);
-                }
+                JObject responseObj = JObject.Parse(item.ToString());
+                string outputInfo = string.Format("Repo Name: {0}, Description: {1}, Created Date: {2}",
+                    (string)responseObj.SelectToken("name"), (string)responseObj.SelectToken("description"),
+                    (string)responseObj.SelectToken("created_at"));
+                lstRepoResults.Items.Add(outputInfo);
             }
-            repoList.Clear();
         }
     }
 }
